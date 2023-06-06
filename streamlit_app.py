6a210bf45b66655ad4592df7445d8b56dd7fa732
MiniUpadhyay07 <116255269+MiniUpadhyay07@users.noreply.github.com>
2023-06-06 14:52:31 +0530

    Update streamlit_app.py
    Added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and blueberry Oatmeal')
+streamlit.text('Smoothie')
+streamlit.text('Juice')
+
