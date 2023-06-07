ce8bdaf974ebe3bcd835175b4cc4af734e491735
MiniUpadhyay07 <116255269+MiniUpadhyay07@users.noreply.github.com>
2023-06-07 10:54:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 #my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Display the table on the page.
- streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(my_fruit_list)
 
 
 
