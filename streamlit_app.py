7e2ff7e40b5dd805e22b3cfe2f43f0029c23f820
MiniUpadhyay07 <116255269+MiniUpadhyay07@users.noreply.github.com>
2023-06-07 10:30:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,3 +13,10 @@ streamlit.title('My Mom\'s New Healthy Dinner')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# Display the table on the page.
+streamlit.dataframe(my_fruit_list)
+
+
+
