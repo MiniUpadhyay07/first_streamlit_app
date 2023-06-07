9697cefb51f7a45630097145edc70fb1ee21fdec
MiniUpadhyay07 <116255269+MiniUpadhyay07@users.noreply.github.com>
2023-06-07 11:18:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,33 +11,37 @@ streamlit.title('My Mom\'s New Healthy Dinner')
 
 
 import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-# Display the table on the page.
-streamlit.dataframe(my_fruit_list)
+#my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+## Display the table on the page.
+#streamlit.dataframe(my_fruit_list)
+#
+#
+#my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+##create index on fruits
+#my_fruit_list = my_fruit_list.set_index('Fruit')
+#streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+## Display the table on the page.
+#streamlit.dataframe(my_fruit_list)
+#
+#
+#my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+##create index on fruits
+#my_fruit_list = my_fruit_list.set_index('Fruit')
+#streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+## Display the table on the page.
+#streamlit.dataframe(my_fruit_list)
+
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 #create index on fruits
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-# Display the table on the page.
-streamlit.dataframe(my_fruit_list)
-
-
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-#create index on fruits
-my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-# Display the table on the page.
-streamlit.dataframe(my_fruit_list)
-
-
-
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
 
 
 
