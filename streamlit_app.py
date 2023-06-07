b2107101275b21dd265144d6c46cba5abba2455a
MiniUpadhyay07 <116255269+MiniUpadhyay07@users.noreply.github.com>
2023-06-07 10:24:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,11 @@
+import streamlit
+streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 and blueberry Oatmeal')
+streamlit.text('🥗 Smoothie')
+streamlit.text('Juice')
+streamlit.text('🥑🍞Avacado toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.title('My Mom\'s New Healthy Dinner')
 
