fba8e8db3489c26b3811eca07ce69613331fbf4c
MiniUpadhyay07 <116255269+MiniUpadhyay07@users.noreply.github.com>
2023-06-07 10:33:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,9 @@ streamlit.title('My Mom\'s New Healthy Dinner')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-# my_fruit_list = my_fruit_list.set_index('Fruit')
+
+#create index on fruits
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Display the table on the page.
 # streamlit.dataframe(my_fruit_list)
