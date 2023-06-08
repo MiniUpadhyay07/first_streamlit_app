75d2ef2785bb7da0f5d96376b12a5d14e1d4cd99
MiniUpadhyay07 <116255269+MiniUpadhyay07@users.noreply.github.com>
2023-06-08 11:31:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -100,7 +100,7 @@ my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from st
 
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
       streamlit.error('"Please select a fruit to get information.")
    else:
