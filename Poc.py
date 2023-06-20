de4dc86ce4fa9ae79fc6dbccfef9f9e0beee18a2
MiniUpadhyay07 <116255269+MiniUpadhyay07@users.noreply.github.com>
2023-06-20 16:07:36 +0530

    Update Poc.py

--- Poc.py
+++ Poc.py
@@ -1,1 +1,13 @@
 
+import streamlit
+import pandas
+import requests
+import snowflake.connector
+#from streamli.error import URLerror
+
+
+streamlit.title('Dashboard')
+
+streamlit.header('Companies')
+streamlit.text('Documents')
+streamlit.text('News')
